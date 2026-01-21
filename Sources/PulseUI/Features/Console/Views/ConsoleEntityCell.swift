a9036a4fd67bd51dfba94d65d3ba4506a4de3849
rUidiO <ruid@stupid.coffee>
2026-01-21 16:39:59 +0800

    change width to 42

--- Sources/PulseUI/Features/Console/Views/ConsoleEntityCell.swift
+++ Sources/PulseUI/Features/Console/Views/ConsoleEntityCell.swift
@@ -17,13 +17,13 @@ struct ConsoleEntityCell: View {
             _ConsoleMessageCell(message: message)
 #if os(macOS)
                 .listRowSeparator(.visible)
-                .frame(height: 40, alignment: .top)
+                .frame(height: 42, alignment: .top)
 #endif
         case .task(let task):
             _ConsoleTaskCell(task: task)
 #if os(macOS)
                 .listRowSeparator(.visible)
-                .frame(height: 40, alignment: .top)
+                .frame(height: 42, alignment: .top)
 #endif
         }
     }
