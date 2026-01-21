d624d1d11e50119bc4e203df0c5b34120d6f41ae
rUidiO <ruid@stupid.coffee>
2026-01-22 00:22:08 +0800

    fix

--- Sources/PulseUI/Features/Inspector/ConnectionInspectorView.swift
+++ Sources/PulseUI/Features/Inspector/ConnectionInspectorView.swift
@@ -22,12 +22,12 @@ struct ConnectionInspectorView: View {
 
     var body: some View {
         VStack(spacing: 0){
-//            VStack(spacing: 0){
-                
-//            }
-//            Divider()
+          
+            
             List {
-                ConnectionTimingView(task: task)
+           
+                    ConnectionTimingView(task: task)
+                
                 contents
             }
             .listStyle(.inset)
@@ -50,9 +50,10 @@ struct ConnectionInspectorView: View {
 //        }
 //        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
 //        .listRowBackground(Color.clear)
-//        
-//    
-
+        
+    
+//            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+//            .listRowBackground(Color.clear)
 
         // Connection Status
         Section {
