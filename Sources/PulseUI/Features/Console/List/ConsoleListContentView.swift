f0578bf40d9d8b31e66da666d4d6a3936910c727
rUidiO <ruid@stupid.coffee>
2026-02-10 08:07:52 +0800

    remove ScrollViewReader

--- Sources/PulseUI/Features/Console/List/ConsoleListContentView.swift
+++ Sources/PulseUI/Features/Console/List/ConsoleListContentView.swift
@@ -12,7 +12,7 @@ struct ConsoleListContentView: View {
     @EnvironmentObject var viewModel: ConsoleListViewModel
 
 #if os(macOS)
-    let proxy: ScrollViewProxy
+//    let proxy: ScrollViewProxy
 
     @SceneStorage("com-github-kean-pulse-is-now-enabled") private var isNowEnabled = true
 #endif
@@ -35,7 +35,7 @@ struct ConsoleListContentView: View {
         } else {
             plainView
 #if os(macOS)
-                .apply(registerNowMode)
+//                .apply(registerNowMode)
 #endif
         }
 #else
@@ -53,7 +53,6 @@ struct ConsoleListContentView: View {
             ForEach(viewModel.visibleEntities, id: \.objectID) { entity in
                 let objectID = entity.objectID
                 ConsoleEntityCell(entity: entity)
-                    .drawingGroup()
                     .id(objectID)
 #if os(iOS) || os(visionOS)
                     .onAppear { viewModel.onAppearCell(with: objectID) }
@@ -88,24 +87,24 @@ struct ConsoleListContentView: View {
     }
 
 #if os(macOS)
-    private func registerNowMode<T: View>(for list: T) -> some View {
-        list.onChange(of: viewModel.entities) { entities in
-            guard isNowEnabled else { return }
-
-            withAnimation {
-                proxy.scrollTo(BottomViewID(), anchor: .top)
-            }
-            // This is a workaround that fixes a scrolling issue when more
-            // than one row is added at the time.
-            DispatchQueue.main.async {
-                proxy.scrollTo(BottomViewID(), anchor: .top)
-            }
-        }
-        .onChange(of: isNowEnabled) {
-            guard $0 else { return }
-            proxy.scrollTo(BottomViewID(), anchor: .top)
-        }
-    }
+//    private func registerNowMode<T: View>(for list: T) -> some View {
+//        list.onChange(of: viewModel.entities) { entities in
+//            guard isNowEnabled else { return }
+//
+//            withAnimation {
+//                proxy.scrollTo(BottomViewID(), anchor: .top)
+//            }
+//            // This is a workaround that fixes a scrolling issue when more
+//            // than one row is added at the time.
+//            DispatchQueue.main.async {
+//                proxy.scrollTo(BottomViewID(), anchor: .top)
+//            }
+//        }
+//        .onChange(of: isNowEnabled) {
+//            guard $0 else { return }
+//            proxy.scrollTo(BottomViewID(), anchor: .top)
+//        }
+//    }
 
     // This view is used to keep scroll to the bottom and keep track of the
     // scroll position (near bottom or not).
--- Sources/PulseUI/Features/Console/List/ConsoleListView.swift
+++ Sources/PulseUI/Features/Console/List/ConsoleListView.swift
@@ -167,17 +167,15 @@ private struct _ConsoleListView: View {
                 ConsoleToolbarView()
             }
             Divider()
-            ScrollViewReader { proxy in
+//            ScrollViewReader { proxy in
                 List(selection: $selection) {
                     if isSearching && !searchViewModel.parameters.isEmpty {
                         ConsoleSearchResultsListContentView()
                     } else {
-                        ConsoleListContentView(proxy: proxy)
-                            .drawingGroup()
-                            
+                        ConsoleListContentView()
                     }
                 }
-            }
+//            }
             .environment(\.defaultMinListRowHeight, 1)
         }
     }
